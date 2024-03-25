c798055349b94e2be526d30b516985d8d59368f6
rukkiecodes <rukkiecodes@gmail.com>
2024-03-25 14:24:23 +0100

    added more fonts

--- index.jsx
+++ index.jsx
@@ -5,7 +5,18 @@ import { View } from 'react-native';
 
 const Font = ({ children, ...props }) => {
     const [fontsLoaded, fontError] = useFonts({
+        'Roboto-Black': require('./fonts/roboto/Roboto-Black.ttf'),
+        'Roboto-BlackItalic': require('./fonts/roboto/Roboto-BlackItalic.ttf'),
+        'Roboto-Bold': require('./fonts/roboto/Roboto-Bold.ttf'),
+        'Roboto-BoldItalic': require('./fonts/roboto/Roboto-BoldItalic.ttf'),
+        'Roboto-Italic': require('./fonts/roboto/Roboto-Italic.ttf'),
+        'Roboto-Light': require('./fonts/roboto/Roboto-Light.ttf'),
+        'Roboto-LightItalic': require('./fonts/roboto/Roboto-LightItalic.ttf'),
+        'Roboto-Medium': require('./fonts/roboto/Roboto-Medium.ttf'),
+        'Roboto-MediumItalic': require('./fonts/roboto/Roboto-MediumItalic.ttf'),
         'Roboto-Regular': require('./fonts/roboto/Roboto-Regular.ttf'),
+        'Roboto-Thin': require('./fonts/roboto/Roboto-Thin.ttf'),
+        'Roboto-ThinItalic': require('./fonts/roboto/Roboto-ThinItalic.ttf'),
     });
 
     const onLayoutRootView = useCallback(async () => {
